3665c4075198979661e45d618a910ea04d0df2bc
VirtuousCrane <ray.daroonpat.2020@gmail.com>
2022-11-14 13:43:04 +0700

    Complete Product

---
+++ src/main/java/com/example/spotify_controller/controller/AppController.java
@@ -0,0 +1,93 @@
+package com.example.spotify_controller.controller;
+
+import com.example.spotify_controller.service.AppService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AppController {
+    private final AppService appService;
+
+    @Autowired
+    public AppController(AppService appService) {
+        this.appService = appService;
+    }
+
+    // ----------------------------- Test Endpoints -----------------------------
+    @GetMapping("/ping")
+    @ResponseBody
+    public String ping() {
+        return "pong";
+    }
+
+    @PostMapping("/ping")
+    @ResponseBody
+    public String postPing() {
+        return "pong, but post";
+    }
+
+    @PutMapping("/ping")
+    @ResponseBody
+    public String putPing() {
+        return "pong, but put";
+    }
+
+    @GetMapping("/echo")
+    @ResponseBody
+    public String echo(@RequestParam String str) {
+        return str;
+    }
+
+    @GetMapping("/auth")
+    @ResponseBody
+    public String auth() {
+        String authURI = appService.getAuthUri();
+        return authURI;
+    }
+
+    // -------------------------- Functional Endpoints --------------------------
+    @PutMapping("/init-access-token")
+    @ResponseBody
+    public String initAuth(@RequestParam String accessToken) {
+        appService.initAccessToken(accessToken);
+        return "Initialized Auth Code";
+    }
+
+    @GetMapping("/get-access-token")
+    @ResponseBody
+    @Deprecated
+    public String saveAuth() {
+        appService.getAccessToken();
+        return "Saved Auth";
+    }
+
+    @GetMapping("/callback")
+    @ResponseBody
+    public String spotifyCallback(@RequestParam String code) {
+        appService.initCode(code);
+        appService.getAccessToken();
+        return "I gotchu fam";
+    }
+
+    @GetMapping("/refresh-access-token")
+    @ResponseBody
+    public String refreshAuth() {
+        appService.refreshCode();
+        return "Refreshed Auth";
+    }
+
+    @GetMapping("/toggle-play")
+    @ResponseBody
+    public String togglePlay() {
+        appService.togglePlay();
+        return "Toggled Play";
+    }
+
+    @GetMapping("/toggle-pause")
+    @ResponseBody
+    public String togglePause() {
+        appService.togglePause();
+        return "Toggled Pause";
+    }
+}
---
+++ src/main/java/com/example/spotify_controller/service/AppService.java
@@ -0,0 +1,122 @@
+package com.example.spotify_controller.service;
+
+import org.springframework.stereotype.Service;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.SpotifyHttpManager;
+import se.michaelthelin.spotify.enums.AuthorizationScope;
+import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRefreshRequest;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+import se.michaelthelin.spotify.requests.data.player.PauseUsersPlaybackRequest;
+import se.michaelthelin.spotify.requests.data.player.StartResumeUsersPlaybackRequest;
+
+import java.net.URI;
+
+@Service
+public class AppService {
+    private static final String clientID = "f9fa2f3c2ff041c3a88230edd63986c3";
+    private static final String clientSecret = "676cd067367748789e75493473fd7da3";
+    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/callback");
+    public static String code = "";
+
+    public static final SpotifyApi spotifyAPI = new SpotifyApi.Builder()
+            .setClientId(clientID)
+            .setClientSecret(clientSecret)
+            .setRedirectUri(redirectUri)
+            .build();
+    public static AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyAPI.authorizationCodeUri()
+            .scope(AuthorizationScope.USER_MODIFY_PLAYBACK_STATE)
+            .build();
+    public static AuthorizationCodeRequest authorizationCodeRequest;
+    public static AuthorizationCodeRefreshRequest authorizationCodeRefreshRequest;
+    public static StartResumeUsersPlaybackRequest startResumeUsersPlaybackRequest;
+    public static PauseUsersPlaybackRequest pauseUsersPlaybackRequest;
+
+    public String getAuthUri() {
+        final URI uri = authorizationCodeUriRequest.execute();
+        System.out.println("URI: " + uri.toString());
+
+        return uri.toString();
+    }
+
+    public void initCode(String code) {
+        this.code = code;
+
+        authorizationCodeRequest  = spotifyAPI.authorizationCode(code)
+                .build();
+        authorizationCodeRefreshRequest = spotifyAPI.authorizationCodeRefresh()
+                .build();
+
+    }
+
+    public void getAccessToken() {
+        try{
+            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+
+            System.out.println(authorizationCodeCredentials.getAccessToken());
+            spotifyAPI.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyAPI.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+
+            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
+
+            startResumeUsersPlaybackRequest = spotifyAPI
+                    .startResumeUsersPlayback()
+                    .build();
+            pauseUsersPlaybackRequest = spotifyAPI
+                    .pauseUsersPlayback()
+                    .build();
+
+        } catch (Exception e) {
+            // NOT YET IMPLEMENTED
+            System.out.println("authCode Error");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void initAccessToken(String accessToken) {
+        spotifyAPI.setAccessToken(accessToken);
+
+        startResumeUsersPlaybackRequest = spotifyAPI
+                .startResumeUsersPlayback()
+                .build();
+        pauseUsersPlaybackRequest = spotifyAPI
+                .pauseUsersPlayback()
+                .build();
+    }
+
+    public void refreshCode() {
+        try {
+            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRefreshRequest.execute();
+
+            spotifyAPI.setAccessToken(authorizationCodeCredentials.getAccessToken());
+
+            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
+        } catch (Exception e) {
+            System.out.println("Refresh Auth Error");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void togglePlay() {
+        try {
+            final String string = startResumeUsersPlaybackRequest.execute();
+
+            System.out.println("Null: " + string);
+        } catch (Exception e) {
+            System.out.println("Toggle Play Error");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void togglePause() {
+        try {
+            final String string = pauseUsersPlaybackRequest.execute();
+
+            System.out.println("Null: " + string);
+        } catch (Exception e) {
+            System.out.println("Toggle Pause Error");
+            System.out.println("Toggle Pause Error");
+        }
+    }
+}
